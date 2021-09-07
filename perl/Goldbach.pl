464a9ec3e0205ca76542b8df5a618c651f7cd3ac
Adrian S. Faircloth <faircloth_as@cobra.cs.mercer.edu>
2021-09-07 11:52:02 -0400

    third commit of first Perl assignment

--- perl/Goldbach.pl
+++ perl/Goldbach.pl
@@ -2,16 +2,25 @@
 use strict;
 use warnings;
 
-for (my $i = 1; $i <= 100; $i++) {
-	print $i;
-	print "\n";
-}
 
 print "Please enter a positive integer: ";
 my $input = <>;
+my $isPrime = 0;
 for (my $i = 1; $i <= $input; $i++){
-	print $i;
-	print "\n";
+	$isPrime = isPrime($i);
+	if ($isPrime == 0) {
+		print $i;
+		print "\n";		
+	}
 }
 
-
+sub isPrime {
+	my $num = shift;
+	my $retVal = 0;
+	for (my $i = 2; $i <= sqrt($num); $i++){
+		if ($num % $i == 0) {
+			$retVal = 1;
+		}
+	}
+	return $retVal;
+}
