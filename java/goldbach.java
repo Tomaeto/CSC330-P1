7a906e8577ac7f8f0d50cd9b937056d3693b0d37
Adrian S. Faircloth <faircloth_as@cobra.cs.mercer.edu>
2021-09-07 10:19:37 -0400

    added function to find all odd primes less than input value

--- java/goldbach.java
+++ java/goldbach.java
@@ -9,18 +9,50 @@ public class goldbach {
 	System.out.print("Enter an odd integer greater than seven: ");
 	val = in.nextInt();
 	meetsReqs = checkReqs(val);
+	int[] primes = findOddPrimes(val);
+	}
 	
-}
-	//Checks if a value satisfies the requirements for Goldbach's weak conjecture (odd int > 7)
 	public static boolean checkReqs(int n) { 
-
+	//Checks if a value satisfies the requirements for Goldbach's weak conjecture (odd int > 7)
 		if(n % 2 == 1 && n > 7)
 		{ 
 			return true;
 		}
 		else
 			return false;
-}
+	}
+	
+	public static int[] findOddPrimes(int n) {
+	//Creates boolean array isPrime[] of all values < n
+	//Any value in isPrime that is not an odd  prime is set to false
+		boolean isPrime[] = new boolean[n];
+		for (int i = 1; i < n; i++)
+			isPrime[i] = true;
 
+		for (int i = 2; i*i < n; i++) {
+			if (isPrime[i] == true) {
+				for (int j = i*i; j < n; j += i)
+					isPrime[j] = false;
+			}
+		}
+		isPrime[2] = false;	//Removing 2 from set of primes	
+		
+	//Counting number of odd primes in range < n
+	//and storing in an int array
+		int numPrimes = 0;
+		for (int i = 0; i < n; i++) {
+			if (isPrime[i] == true)
+				numPrimes++;
+		}
+		int[] primeVals = new int[numPrimes];
+		int pointer = 0;
+		for (int i = 0; i < n; i++) {
+			if (isPrime[i] == true) {
+				primeVals[pointer] = i;
+				pointer++;
+			}
+		}
+		return primeVals;
+	}
 	
 }
