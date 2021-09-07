febcf3530f876dafe2b2dd7fcf3f48fef4de5726
Adrian S. Faircloth <faircloth_as@cobra.cs.mercer.edu>
2021-09-07 11:36:29 -0400

    second commit of first Goldbach assignment

--- perl/Goldbach.pl
+++ perl/Goldbach.pl
@@ -6,3 +6,12 @@ for (my $i = 1; $i <= 100; $i++) {
 	print $i;
 	print "\n";
 }
+
+print "Please enter a positive integer: ";
+my $input = <>;
+for (my $i = 1; $i <= $input; $i++){
+	print $i;
+	print "\n";
+}
+
+
