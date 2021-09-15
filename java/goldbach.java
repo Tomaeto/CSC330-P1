11d30517d43f022310c7776ad3d975dd634a3f84
Adrian S. Faircloth <faircloth_as@cobra.cs.mercer.edu>
2021-09-14 20:01:10 -0400

    removed further redundancies

--- java/goldbach.java
+++ java/goldbach.java
@@ -112,7 +112,7 @@ private static int[] triplet = new int[3];
 				if(inPrimeSet(i) == true && inPrimeSet(j) == true)
 				{
 					k = sum - (j + i);
-					if  (k != j && j != i && k != i)
+					if  (k != j && k != i)
 					{
 						currNorm = Math.sqrt((i*i) + (j*j) + (k*k));
 						if (smallestNorm == 0 || currNorm < smallestNorm)
