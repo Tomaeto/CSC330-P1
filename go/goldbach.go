b5169317cdd415e750633fe03751dc3bfda6df4a
Adrian S. Faircloth <faircloth_as@cobra.cs.mercer.edu>
2021-09-26 21:56:49 -0400

    added comments to Go version of program

--- go/goldbach.go
+++ go/goldbach.go
@@ -1,9 +1,14 @@
 package main
-
+//Program to compute Goldbach's Weak Conjecture for input value
+//For any odd int > 7, finds triplet of primes w/ smallest vector norm that sum to that value
+//By Adrian Faircloth
+//09-16-21
 import "fmt"
 import "math"
 import "sort"
 func findPrimes(val int) ([]int) {
+	//Finds all odd primes < input value
+	//Returns array of found primes
 	var primes []int;
 	var i int;
 	var j int;
@@ -23,6 +28,8 @@ func findPrimes(val int) ([]int) {
 } 
 
 func inSet(val int, set []int) (int) {
+	//Checks if an int is in an array of ints
+	//Used to check if a value is in the set of primes < input value
 	for i, item := range set {
 		if item == val {
 			return i;
@@ -32,6 +39,10 @@ func inSet(val int, set []int) (int) {
 }
 
 func findTriplet(sum int, set *[]int) ([3]int) {
+	//Finds triplet of primes < input whose vector norm is smallest
+	//Compares the norm of each triplet found to current smallest norm
+	//If current norm < smallest norm, fills triplet array with those values
+	//Finally, triplet w/ smallest possible vector norm is stored in array and returned
 	var triplet [3] int;
 	var i int;
 	var j int;
@@ -58,6 +69,10 @@ func findTriplet(sum int, set *[]int) ([3]int) {
 }
 
 func main() {
+	//Takes user input inpVal and checks if inpVal meets Goldbach requirements
+	//If yes, finds primes < inpVal, finds triplet w/ smallest norm that sums to inpVal,
+	//	sorts triplet and prints
+	//If no, prints error message and exits
 	var inpVal int;
 	fmt.Print("Enter an odd integer greater than seven: ");
 	fmt.Scanln(&inpVal);
